b08f2b92dfc98cd1eac681450162ca98f8910f84
mjwebb1993 <mjwebb1993@yahoo.com>
2023-10-11 19:55:08 -0500

    Finished 8.2

--- server/app.js
+++ server/app.js
@@ -1,7 +1,40 @@
 // 'Import' the Express module instead of http
 import express from "express";
+import dotenv from "dotenv";
 // Initialize the Express application
 const app = express();
+
+// Load environment variables from .env file
+dotenv.config();
+
+const PORT = process.env.PORT || 4040;
+
+const logging = (request, response, next) => {
+  console.log(
+    `${request.method} ${request.url} ${new Date().toLocaleString("en-us")}`
+  );
+
+  next();
+};
+
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
+
+app.use(cors);
+app.use(express.json());
+app.use(logging);
 
 // Handle the request with HTTP GET method from http://localhost:4040/status
 app.get("/status", (request, response) => {
@@ -58,4 +91,4 @@ app.get("/weather/:city", (request, response) => {
 
 // Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
-app.listen(4040, () => console.log("Listening on port 4040"));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
